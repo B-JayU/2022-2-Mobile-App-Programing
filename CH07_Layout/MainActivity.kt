933790b68f516032fc5f72a5376796518e233b92
B-JayU <qudwn8712@naver.com>
2022-11-30 13:37:14 +0900

    Add files via upload

---
+++ CH07_Layout/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ch7_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ch7_layout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
